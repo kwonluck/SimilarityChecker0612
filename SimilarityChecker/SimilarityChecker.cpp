bec53368186cfc1ad0ccec52a80adcf0ae4b346f
kwonluck <software_k@naver.com>
2024-05-31 11:30:47 +0900

    Alpha Point 기능 구현 및 TC 추가

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <set>
 using namespace std;
 
 
@@ -15,18 +16,31 @@ public:
 			return MAX_LENGTH_POINT;
 
 		if (isStringLegnthTwice())
-			return MIN_LENGTH_POINT;
+			return NO_POINT;
 
 		return getLengthPartialPoint();
 	}
 
 	int getAlphaPoint() {
+		int totalCnt = getTotalCharCnt();
+		int sameCnt = getSameCharCnt();
+
+		if (sameCnt == 0)
+			return NO_POINT;
+
+		if (totalCnt == sameCnt)
+			return MAX_ALPHA_POINT;
+		
+		return sameCnt / totalCnt * MAX_ALPHA_POINT;
+
 		return 0;
 	}
 
 private:
 	const int MAX_LENGTH_POINT = 60;
-	const int MIN_LENGTH_POINT = 0;
+	const int MAX_ALPHA_POINT = 40;
+	const int NO_POINT = 0;
+
 	pair<string, string> compareStringSet;
 
 	bool isSameStringLength() {
@@ -54,5 +68,29 @@ private:
 			denominator = compareStringSet.first.length();
 
 		return (1 - (abs(gap) / denominator)) * MAX_LENGTH_POINT;
+	}
+
+	int getTotalCharCnt() {
+		set<char> charList;
+		for (auto ch : compareStringSet.first)
+			charList.insert(ch);
+		for (auto ch : compareStringSet.second)
+			charList.insert(ch);
+
+		return charList.size();
+	}
+
+	int getSameCharCnt() {
+		set<char> sameCharList;
+		for (auto ch1 : compareStringSet.first) {
+			for (auto ch2 : compareStringSet.second) {
+				if (ch1 == ch2)	{
+					sameCharList.insert(ch1);
+					break;
+				}
+			}
+		}
+
+		return sameCharList.size();
 	}
 };
